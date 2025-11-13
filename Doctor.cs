59b34d8311f27662df2167f934dc97b6d2d9aab3
vartanchik12 <147078735+vartanchik12@users.noreply.github.com>
2025-11-13 16:46:38 +0400

    Add files via upload

---
+++ Doctor.cs
@@ -0,0 +1,37 @@
+﻿namespace Hospital.Domain;
+
+/// <summary>
+/// Represents a medical doctor.
+/// </summary>
+public class Doctor
+{
+    /// <summary>
+    /// Unique identifier for the doctor.
+    /// </summary>
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Passport number of the doctor.
+    /// </summary>
+    public required string Passport { get; set; }
+
+    /// <summary>
+    /// Full name of the doctor.
+    /// </summary>
+    public required string FullName { get; set; }
+
+    /// <summary>
+    /// Year of birth of the doctor.
+    /// </summary>
+    public required DateOnly BirthDate { get; set; }
+
+    /// <summary>
+    /// Medical specialization of the doctor.
+    /// </summary>
+    public required Specialization Specialization { get; set; }
+
+    /// <summary>
+    /// Years of work experience.
+    /// </summary>
+    public required int WorkExperience { get; set; }
+}
